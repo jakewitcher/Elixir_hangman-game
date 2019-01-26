40e4ffbde6a612a22bfaba972a576eb98dc0a247
Jake Witcher <jjwitcher09@gmail.com>
2019-01-26 13:47:05 -0500

    fix hangman api, adjust tests

--- hangman/test/game_test.exs
+++ hangman/test/game_test.exs
@@ -16,37 +16,43 @@ defmodule GameTest do
     for state <- [ :won, :lost ] do
       game = Game.new_game() 
             |> Map.put(:game_state, state) 
-      assert ^game = Game.make_move(game, "x")
+      assert {^game, _} = Game.make_move(game, "x")
     end
   end
 
   test "first occurence of letter is not already used" do 
-    game = Game.new_game() 
+    { game = %{ game_state: state }, _} = 
+    Game.new_game() 
     |> Game.make_move("x") 
-    assert game.game_state != :already_used 
+    assert state != :already_used 
   end
 
   test "second occurence of letter is already used" do 
-    game = Game.new_game() 
+    { game = %{ game_state: state }, _} = 
+    Game.new_game() 
     |> Game.make_move("x") 
-    assert game.game_state != :already_used
-    game = Game.make_move(game, "x") 
-    assert game.game_state == :already_used  
+    assert state != :already_used
+
+    { game = %{ game_state: state }, _} = 
+    Game.make_move(game, "x") 
+    assert state == :already_used  
   end
 
   test "a good guess is recognized" do 
     game = Game.new_game("wibble") 
     |> Game.make_move("w")
-    assert game.game_state == :good_guess 
-    assert game.turns_left == 7 
+    { game = %{ game_state: state, turns_left: turns }, _} = game
+    assert state == :good_guess 
+    assert turns == 7 
   end
 
   test "a good guess is a won game" do 
     game = Game.new_game("wibble") 
     |> Map.put(:used, MapSet.new(["w", "i", "b", "l"]))
     game = Game.make_move(game, "e")
-    assert game.game_state == :won 
-    assert game.turns_left == 7 
+    { game = %{ game_state: state, turns_left: turns }, _} = game
+    assert state == :won 
+    assert turns == 7 
   end
 
   test "a good guess wins the game" do
@@ -54,8 +60,8 @@ defmodule GameTest do
     game = Game.new_game("wibble")
     game = Enum.reduce(good_moves, game, 
       fn (move, game) -> 
-        game = Game.make_move(game, move)
-        game
+        {game, _} = Game.make_move(game, move)
+        game 
       end 
     )
     assert game.game_state == :won 
@@ -64,16 +70,18 @@ defmodule GameTest do
 
   test "a bad guess is recognized" do 
     game = Game.new_game("wibble") 
-    |> Game.make_move("x") 
-    assert game.game_state == :bad_guess
-    assert game.turns_left == 6
+    |> Game.make_move("x")
+    { game = %{ game_state: state, turns_left: turns }, _} = game 
+    assert state == :bad_guess
+    assert turns == 6
   end
 
   test "a bad guess has lost the game" do 
     game = Game.new_game("wibble") 
     |> Map.put(:turns_left, 1) 
-    game = Game.make_move(game, "x") 
-    assert game.game_state == :lost
+    game = Game.make_move(game, "x")
+    { game = %{ game_state: state }, _} = game
+    assert state == :lost
   end
 
   test "a bad guess loses the game" do
@@ -81,7 +89,7 @@ defmodule GameTest do
     game = Game.new_game("wibble")
     game = Enum.reduce(bad_moves, game, 
       fn (move, game) -> 
-        game = Game.make_move(game, move)
+        {game, _} = Game.make_move(game, move)
         game
       end 
     )
@@ -89,8 +97,4 @@ defmodule GameTest do
     assert game.turns_left == 1 
   end
 
-  # test "a vaild character returns true" do 
-  #   input = "x"
-  #   assert Game.is_valid?(input) == input
-  # end 
 end
